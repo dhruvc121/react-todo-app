dd9c0757d8af65810027d4cb324efdf679f33567
dhruvc121 <dhruvc121@gmail.com>
2020-10-02 11:57:33 +0530

    Updates

---
+++ precache-manifest.c82ac4009bc05499ea0c4a519a2f21e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db0a204e5dc4e7cc10976857c145783d",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "46185be82bb1bd0311d9",
+    "url": "/react-todo-app/static/css/main.369dbbd5.chunk.css"
+  },
+  {
+    "revision": "5d033dad9833ea0b300f",
+    "url": "/react-todo-app/static/js/2.fbd27ce8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-todo-app/static/js/2.fbd27ce8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46185be82bb1bd0311d9",
+    "url": "/react-todo-app/static/js/main.764dd458.chunk.js"
+  },
+  {
+    "revision": "3d1e41507955c947834a",
+    "url": "/react-todo-app/static/js/runtime-main.b9410e1b.js"
+  }
+]);
